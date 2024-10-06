f3b91d5bcf125be9ba27df91e69dc13b493badc3
jwp1000 <jepurser@gmail.com>
2024-10-06 15:49:15 -0500

    Image exercise part two

---
+++ methods/images.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"golang.org/x/tour/pic"
+	"image"
+	"image/color"
+)
+
+type Image struct{}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+	return image.Rect(0, 0, 100, 100)
+}
+
+func (i Image) At(x, y int) color.Color {
+	return color.RGBA{uint8(x), uint8(y), uint8(y), uint8(y)}
+}
+
+func main() {
+	m := Image{}
+	pic.ShowImage(m)
+}
