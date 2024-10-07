fdfa85de0700dfa2198460c070f1bcf4ca336f64
jwp1000 <jepurser@gmail.com>
2024-10-07 11:08:44 -0500

    Concurrency -first

---
+++ concurrency/default_selection.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
+	for {
+		select {
+		case val := <-tick:
+			fmt.Println(val)
+			fmt.Println("tick.")
+		case val := <-boom:
+			fmt.Println(val)
+			fmt.Println("BOOM!")
+			return
+		default:
+			fmt.Println("    .")
+			time.Sleep(50 * time.Millisecond)
+		}
+	}
+}
