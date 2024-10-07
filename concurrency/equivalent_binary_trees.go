0f997ac052aa580b5c825cdafb032f49bad357dd
jwp1000 <jepurser@gmail.com>
2024-10-07 11:22:07 -0500

    Binary tree - test for sameness

--- concurrency/equivalent_binary_trees.go
+++ concurrency/equivalent_binary_trees.go
@@ -12,22 +12,44 @@ func Walk(t *tree.Tree, ch chan int) {
 		Walk(t.Left, ch)
 	}
 	ch <- t.Value
-	//fmt.Println("val:", t.Value)
 	if t.Right != nil {
 		Walk(t.Right, ch)
 	}
 }
 
+func WalkThenClose(treep *tree.Tree, ch chan int) {
+	Walk(treep, ch)
+	close(ch)
+}
+
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-//func Same(t1, t2 *tree.Tree) bool
+func Same(t1, t2 *tree.Tree) bool {
+	ch1, ch2 := make(chan int), make(chan int)
+	go WalkThenClose(t1, ch1)
+	go WalkThenClose(t2, ch2)
+
+	for v1 := range ch1 {
+		v2 := <-ch2
+		fmt.Println("v1:", v1, ", v2:", v2)
+		if v1 != v2 {
+			return false
+		}
+	}
+
+	return true
+}
 
 func main() {
-	ch := make(chan int)
-	t1 := tree.New(1)
-	fmt.Println(t1)
-	go Walk(t1, ch)
-	for i := 0; i < 10; i++ {
-		fmt.Println(<-ch)
-	}
+	t1, t2 := tree.New(1), tree.New(1)
+	fmt.Println("t1:", t1)
+	fmt.Println("t2:", t2)
+	test := Same(t1, t2)
+	fmt.Println("Same? ", test)
+	t1, t2 = tree.New(1), tree.New(2)
+	fmt.Println("t1:", t1)
+	fmt.Println("t2:", t2)
+	test = Same(t1, t2)
+	fmt.Println("Same? ", test)
+
 }
